625a2cc06d0286a67a1a4a5ee6b7eafd339d677a
TatyCat <williams.tatyanna@gmail.com>
2018-03-07 18:17:24 -0500

    Redo of Ch 2 Prep Exercise EvenNumb List With Class

---
+++ EvenListMaker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ch2PrepExercises
+{
+    class EvenListMaker
+    {
+        public static string EvenList(List<int> numList)
+        {
+            //running vic solution, replace string with void 
+            List<int> allEven = new List<int>();
+
+            //List<int> results = numList.FindAll(number => number % 2 == 0);
+            //results.ForEach(result => Console.WriteLine(result));
+
+            foreach (int numb in numList)
+            {
+                if (numb % 2 == 0)
+                {
+                    allEven.Add(numb);
+                }
+
+            }
+
+            string evenNumbString = string.Join(",", allEven.ToArray());
+
+            return evenNumbString;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ch2PrepExercises
+{
+    class Program
+    {
+        public static void Main()
+        {
+            List<int> numList = new List<int>{ 4, 3, 2, 1, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine(EvenListMaker.EvenList(numList));
+            //EvenListMaker.EvenList(numList);
+            Console.ReadLine();
+        }
+            
+    }
+}
